0645fb9d35688919075606bbaeefeb5e7549c68c
DiwsgenLop <diwsgen.lopez@alumno.buap.mx>
2024-02-10 21:59:26 -0600

    Formulario de maestro

--- src/app/partials/registro-maestros/registro-maestros.component.ts
+++ src/app/partials/registro-maestros/registro-maestros.component.ts
@@ -1,10 +1,104 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { MaestroService } from 'src/app/services/maestro.service';
+declare var $:any; //Declares a variable that can be used anywhere in the component
 @Component({
   selector: 'app-registro-maestros',
   templateUrl: './registro-maestros.component.html',
   styleUrls: ['./registro-maestros.component.scss']
 })
-export class RegistroMaestrosComponent {
+export class RegistroMaestrosComponent implements OnInit{
+  @Input() rol:string = "";
 
+  public maestro:any = {};
+  public editar:boolean = false;
+  public errors:any = {};
+  //Para contraseñas
+  public hide_1: boolean = false;
+  public hide_2: boolean = false;
+  public inputType_1: string = 'password';
+  public inputType_2: string = 'password';
+  //Materias
+  public materias:any[]= [
+    {value: '1', nombre: 'Aplicaciones Web'},
+    {value: '2', nombre: 'Programación 1'},
+    {value: '3', nombre: 'Bases de datos'},
+    {value: '4', nombre: 'Tecnologías Web'},
+    {value: '5', nombre: 'Minería de datos'},
+    {value: '6', nombre: 'Desarrollo móvil'},
+    {value: '7', nombre: 'Estructuras de datos'},
+    {value: '8', nombre: 'Administración de redes'},
+    {value: '9', nombre: 'Ingeniería de Software'},
+    {value: '10', nombre: 'Administración de S.O.'},
+  ];
+
+  constructor(
+    private maestroService: MaestroService
+  ){}
+
+  ngOnInit(): void {
+    //Con esto nosotros podemos obtener el esquema de maestro es decir los campos que necesitamos para registrar un maestro
+    this.maestro = this.maestroService.esquemaMaestro();
+    this.maestro.rol = this.rol; //Asignamos el rol (Admin, Maestro, Alumno)
+    console.log("Maestro: ", this.maestro); //Imprimimos el esquema de maestro
+  }
+
+  public regresar(){
+
+  }
+
+  public registrar(){
+    //Validar
+    this.errors = [];
+    
+    this.errors = this.maestroService.validarMaestro(this.maestro, this.editar);
+    if(!$.isEmptyObject(this.errors)){
+      return false;
+    }
+
+    //TODO: Después registraremos maestro
+  }
+
+  public actualizar(){
+
+  }
+
+  //Funciones para password
+  showPassword()
+  {
+    if(this.inputType_1 == 'password'){
+      this.inputType_1 = 'text';
+      this.hide_1 = true;
+    }
+    else{
+      this.inputType_1 = 'password';
+      this.hide_1 = false;
+    }
+  }
+
+  showPwdConfirmar()
+  {
+    if(this.inputType_2 == 'password'){
+      this.inputType_2 = 'text';
+      this.hide_2 = true;
+    }
+    else{
+      this.inputType_2 = 'password';
+      this.hide_2 = false;
+    }
+  }
+  //Función para checkbox
+  public checkboxChange(event:any){
+    //console.log("Evento: ", event);
+    if(event.checked){
+      this.maestro.materias_json.push(event.source.value)
+    }else{
+      console.log(event.source.value);
+      this.maestro.materias_json.forEach((materia, i) => {
+        if(materia == event.source.value){
+          this.maestro.materias_json.splice(i,1)
+        }
+      });
+    }
+    console.log("Array materias: ", this.maestro);
+  }
 }
---
+++ src/app/services/maestro.service.ts
@@ -0,0 +1,103 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ErrorsService } from './tools/errors.service';
+import { ValidatorService } from './tools/validator.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MaestroService {
+
+  constructor(
+    private http: HttpClient,
+    private validatorService: ValidatorService,
+    private errorService: ErrorsService,
+  ) { }
+
+  //Esquema para maestro
+  public esquemaMaestro() {
+    return {
+      'rol': '',
+      'id_trabajador': '',
+      'first_name': '',
+      'last_name': '',
+      'email': '',
+      'password': '',
+      'confirmar_password': '',
+      'telefono': '',
+      'rfc': '',
+      'birthday': '',
+      'cubiculo': '',
+      'area_investigacion': '',
+      'materias': []
+    }
+  }
+
+  //Validación para el formulario
+  public validarMaestro(data: any, editar: boolean) {
+    console.log("Validando maestro... ", data);
+    let error: any = [];
+    //Validar campos
+    // id_trabajador
+    if(!this.validatorService.required(data["id_trabajador"])){
+      error["id_trabajador"] = this.errorService.required;
+    }
+    //Nombre
+    if(!this.validatorService.required(data["first_name"])){
+      error["first_name"] = this.errorService.required;
+    }
+    //Apellido
+    if(!this.validatorService.required(data["last_name"])){
+      error["last_name"] = this.errorService.required;
+    }
+    //Email necesita 3 validaciones, primera de requerido, 2da de longitud máxima y 3ra de formato de email
+    if(!this.validatorService.required(data["email"])){
+      error["email"] = this.errorService.required;
+    }else if(!this.validatorService.max(data["email"], 40)){
+      error["email"] = this.errorService.max(40);
+    }else if (!this.validatorService.email(data['email'])) {
+      error['email'] = this.errorService.email;
+    }
+    //la variable editar es para saber si se está editando o no, si no se está editando se valida la contraseña
+    if(!editar){
+      if(!this.validatorService.required(data["password"])){
+        error["password"] = this.errorService.required;
+      }
+      if(!this.validatorService.required(data["confirmar_password"])){
+        error["confirmar_password"] = this.errorService.required;
+      }
+    }
+    //Fecha de nacimiento
+    if(!this.validatorService.required(data["birthday"])){
+      error["birthday"] = this.errorService.required;
+    }
+    //Teléfono
+    if(!this.validatorService.required(data["telefono"])){
+      error["telefono"] = this.errorService.required;
+    }
+    //RFC, primero es requerido y después se valida el formato
+    if(!this.validatorService.required(data["rfc"])){
+      error["rfc"] = this.errorService.required;
+    }else if(!this.validatorService.min(data["rfc"], 12)){
+      error["rfc"] = this.errorService.min(12);
+      alert("La longitud de caracteres deL RFC es menor, deben ser 12");
+    }else if(!this.validatorService.max(data["rfc"], 13)){
+      error["rfc"] = this.errorService.max(13);
+      alert("La longitud de caracteres deL RFC es mayor, deben ser 13");
+    }
+    //cubiculo, requerido y solo en formato numérico
+    if(!this.validatorService.required(data["cubiculo"])){
+      error["cubiculo"] = this.errorService.required;
+    }else if(!this.validatorService.numeric(data["cubiculo"])){
+      alert("El formato es solo números");
+    }
+    //area_investigacion
+
+    //materias, se valida que no esté vacío
+    if(data["materias"].length == 0){
+      error["materias"] = this.errorService.required;
+    }
+    //Regresar errores
+    return error;
+  }
+}
