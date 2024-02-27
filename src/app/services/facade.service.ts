73f9683b329c55d377426388007568f4f0fc3ba9
DiwsgenLop <diwsgen.lopez@alumno.buap.mx>
2024-02-26 18:22:14 -0600

    Verificacion de login con cadena

--- src/app/services/facade.service.ts
+++ src/app/services/facade.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ErrorsService } from './tools/errors.service';
+import { ValidatorService } from './tools/validator.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacadeService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    public router: Router,
+    private validatorService: ValidatorService,
+    private errorService: ErrorsService,
+  ) { }
+
+  //Generaremos un validar login
+  public validarLogin(username: String, password: String) {
+    var data ={
+      "username": username,
+      "password": password
+    }
+    console.log("Validando login... ", data);
+    let error :  any = [];
+    
+    if(!this.validatorService.required(data["username"])){
+      error["username"] = this.errorService.required;
+    }else if(!this.validatorService.max(data["username"],40)){
+      error["username"]=this.errorService.max(40);
+    }else if(!this.validatorService.email(data["username"])){
+      error["username"]=this.errorService.email;
+    }
+
+    if(!this.validatorService.required(data["password"])){
+      error["password"]=this.errorService.required
+    }
+    return error;
+  }
 }
